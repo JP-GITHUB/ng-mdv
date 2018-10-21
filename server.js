bd2e4cc9216c1979803f043ab105436f4472c5f7
JP-GITHUB <jose.luis.p10@hotmail.com>
2018-10-21 00:29:00 -0300

    Retry fix error package.json

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const path = require('path');
 
+const path = require('path');
+const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/ng-mdv'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/ng-mdv'));
 
+// Send all requests to index.html
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/ng-mdv/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ng-mdv/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
