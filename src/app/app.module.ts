2a679f7ba5e94082b8bd2faf88734a1402446f44
JP-GITHUB <jose.luis.p10@hotmail.com>
2018-10-20 23:42:27 -0300

    Landding v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ParallaxHomeComponent } from './shared/parallax-home/parallax-home.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ParallaxHomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
