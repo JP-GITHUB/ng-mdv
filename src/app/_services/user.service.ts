0201d771fda80a8ca436096ea9ed9daee1f3c163
JP-GITHUB <jose.luis.p10@hotmail.com>
2018-10-28 00:14:12 -0300

    Optimizacion listar

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { DataTablesResponse } from '../_classes/data-tables-response';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +11,14 @@ export class UserService {
   constructor(
     private http: HttpClient
   ) { }
+
+  getDatatablesData(dataTablesParameters) {
+    return this.http
+      .post<DataTablesResponse>(
+        'http://localhost:3000/users/datatables',
+        dataTablesParameters, {}
+      )
+  }
 
   registry(form: any) {
     return this.http.post('http://localhost:3000/users/register', form);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-
-class Person {
-  id: number;
-  name: string;
-  lastname: string;
-}
-
-class DataTablesResponse {
-  data: any[];
-  draw: number;
-  recordsFiltered: number;
-  recordsTotal: number;
-}
+import { User } from '../_classes/user';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -21,32 +9,30 @@ class DataTablesResponse {
 })
 export class UserComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
-  persons: Person[];
+  users: User[];
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
     const that = this;
 
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength: 2,
+      pageLength: 10,
       serverSide: true,
       processing: true,
       ajax: (dataTablesParameters: any, callback) => {
-        that.http
-          .post<DataTablesResponse>(
-            'http://localhost:3000/users/datatables',
-            dataTablesParameters, {}
-          ).subscribe(resp => {
-            that.persons = resp.data;
+        this.userService.getDatatablesData(dataTablesParameters).subscribe(resp => {
+          that.users = resp.data;
 
-            callback({
-              recordsTotal: resp.recordsTotal,
-              recordsFiltered: resp.recordsFiltered,
-              data: []
-            });
+          callback({
+            recordsTotal: resp.recordsTotal,
+            recordsFiltered: resp.recordsFiltered,
+            data: []
           });
+        });
       },
       columns: [{ data: 'id' }, { data: 'name' }, { data: 'lastname' }]
     };
