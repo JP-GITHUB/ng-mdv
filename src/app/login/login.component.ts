5584471f8800e50bd02c12b0aabd0ca96560d4da
JP-GITHUB <jose.luis.p10@hotmail.com>
2018-10-28 22:19:02 -0300

    Validacion mensaje de login invalido

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
+import { NotifierService } from 'angular-notifier';
+
 import { NavbarService } from '../_services/navbar.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -12,6 +14,8 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private readonly notifier: NotifierService;
+
   loginForm: FormGroup;
   loading = false;
   submitted = false;
@@ -22,8 +26,11 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService
-  ) { }
+    private authService: AuthService,
+    notifierService: NotifierService
+  ) {
+    this.notifier = notifierService;
+  }
 
   ngOnInit() {
     this.navbarService.controlStatus(false);
@@ -49,10 +56,17 @@ export class LoginComponent implements OnInit {
       .pipe(first())
       .subscribe(
         data => {
-          this.router.navigate(["/"]);
+          if (data['status']) {
+            this.router.navigate(["/"]);
+          } else {
+            this.notifier.notify('error', data['msg']);
+          }
+
+          this.loading = false;
         },
         error => {
           this.loading = false;
+          this.notifier.notify('error', 'Error al ingresar.');
         });
   }
 
