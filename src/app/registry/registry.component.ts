9d0b975136d9eb9087ebd64e8b3b54a32566dcce
crisgg <maxcorreoimo@gmail.com>
2018-12-03 16:53:51 -0300

    Validado formulario de registro menos correo

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { UserService } from '../_services/user.service';
 
 import { NotifierService } from 'angular-notifier';
@@ -11,42 +11,109 @@ import { NotifierService } from 'angular-notifier';
 })
 export class RegistryComponent implements OnInit {
   private readonly notifier: NotifierService;
-
   registryForm: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: String;
+  validRut = false;
 
   constructor(
-    private formBuilder: FormBuilder,
     private userService: UserService,
-    notifierService: NotifierService
+    notifierService: NotifierService,
+    
   ) {
     this.notifier = notifierService;
   }
 
   ngOnInit() {
-    this.registryForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      lastname: ['', Validators.required],
-      rut: ['', Validators.required],
-      mail: ['', Validators.required],
-      telephone: ['', Validators.required],
-      password: ['', Validators.required],
-      repassword: ['', Validators.required]
+    this.registryForm = new FormGroup({
+      'name': new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20)
+      ]),
+      'lastname': new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20)
+      ]),
+      'rut': new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(13),
+      ]),
+      'mail': new FormControl('', [
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(100)
+      ]),
+      'telephone': new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(14)
+      ]),
+      'password': new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ]),
+      'repassword': new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ]),
     });
   }
 
-  get f() { return this.registryForm.controls; }
+  get f() { return this.registryForm.controls;}
 
+  checkRut(rut) {
+
+    var valor = rut.value.replace('.','');
+    valor = valor.replace('-','');
+    
+    let cuerpo = valor.slice(0,-1);
+    let dv = valor.slice(-1).toUpperCase();
+    
+    rut.value = cuerpo + '-'+ dv
+    
+    if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+    
+    let suma = 0;
+    let multiplo = 2;
+    
+    for(let i=1;i<=cuerpo.length;i++) {    
+        let index = multiplo * valor.charAt(cuerpo.length - i);        
+        suma = suma + index;
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }  
+    }
+    
+    let dvEsperado = 11 - (suma % 11);
+    
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 0)?11:dv;
+    
+    if(dvEsperado != dv) { return false; }
+    
+    return true;
+}
+  
   onSubmit() {
     let data;
     this.submitted = true;
-
     if (this.registryForm.invalid) {
+      this.notifier.notify('error', 'Uno de los campos ingresados no es correcto');
       console.log("Invalid form");
       return;
     }
+
+    if(!this.checkRut(this.f.rut)){
+      this.validRut = true;
+      this.f.rut.setErrors({"invalidRut": true})
+      this.notifier.notify('error', 'El rut ingresado no es valido');
+      return;
+    }
+
 
     this.loading = true;
 
