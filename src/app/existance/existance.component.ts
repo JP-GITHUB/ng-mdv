345736f37414ae5d662d4f3784a1ae099079e846
JP-GITHUB <joseluis.p10@hotmail.com>
2018-12-04 02:06:38 -0300

    Traduccion de datatables a español

--- src/app/existance/existance.component.ts
+++ src/app/existance/existance.component.ts
@@ -36,6 +36,28 @@ export class ExistanceComponent implements OnInit {
       pageLength: 10,
       serverSide: true,
       processing: true,
+      language: {
+        processing: "Procesando...",
+        search: "Buscar:",
+        lengthMenu: "Mostrar _MENU_ registros",
+        info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
+        infoEmpty: "Mostrando ningún elemento.",
+        infoFiltered: "(filtrado _MAX_ elementos total)",
+        infoPostFix: "",
+        loadingRecords: "Cargando registros...",
+        zeroRecords: "No se encontraron registros",
+        emptyTable: "No hay datos disponibles en la tabla",
+        paginate: {
+          first: "Primero",
+          previous: "Anterior",
+          next: "Siguiente",
+          last: "Último"
+        },
+        aria: {
+          sortAscending: ": Activar para ordenar la tabla en orden ascendente",
+          sortDescending: ": Activar para ordenar la tabla en orden descendente"
+        }
+      },
       ajax: (dataTablesParameters: any, callback) => {
         this.existanceService.getDatatablesData(dataTablesParameters).subscribe(resp => {
           that.existances = resp.data;
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -36,6 +36,28 @@ export class ProductComponent implements OnInit {
       pageLength: 10,
       serverSide: true,
       processing: true,
+      language: {
+        processing: "Procesando...",
+        search: "Buscar:",
+        lengthMenu: "Mostrar _MENU_ registros",
+        info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
+        infoEmpty: "Mostrando ningún elemento.",
+        infoFiltered: "(filtrado _MAX_ elementos total)",
+        infoPostFix: "",
+        loadingRecords: "Cargando registros...",
+        zeroRecords: "No se encontraron registros",
+        emptyTable: "No hay datos disponibles en la tabla",
+        paginate: {
+          first: "Primero",
+          previous: "Anterior",
+          next: "Siguiente",
+          last: "Último"
+        },
+        aria: {
+          sortAscending: ": Activar para ordenar la tabla en orden ascendente",
+          sortDescending: ": Activar para ordenar la tabla en orden descendente"
+        }
+      },
       ajax: (dataTablesParameters: any, callback) => {
         this.productService.getDatatablesData(dataTablesParameters).subscribe(
           resp => {
